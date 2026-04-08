82253d6466b6e3d4b4cc0a70623a1f5c123520b4
radiantbtf <kleverxumr@gmail.com>
2026-04-09 00:20:37 +0200

    Small changes

--- src/jdm/repository/PatientRepository.java
+++ src/jdm/repository/PatientRepository.java
@@ -1,4 +1,4 @@
-package jdm.repository;
+'''package jdm.repository;
 
 import jdm.model.Patient;
 import java.util.List;
@@ -9,4 +9,17 @@ public interface PatientRepository {
     List<Patient> findAll();
     void update(Patient patient);
     void delete(int id);
+}'''
+
+package jdm.repository;
+
+import jdm.model.Patient;
+import java.util.List;
+
+public interface PatientRepository {
+    boolean save(Patient patient);
+    Patient findById(int id);
+    List<Patient> findAll();
+    boolean update(Patient patient);
+    boolean delete(int id);
 }
