0faae2c561d028352838182c2dc3d08779314bfc
LeiKei <92036191+RGB-LeiKei@users.noreply.github.com>
2026-04-17 09:20:10 +0200

    Fixed sh file

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd "$(dirname "$0")"
+mkdir -p out
 javac -cp "lib/*" -d out src/jdm/Main.java src/jdm/model/*.java src/jdm/repository/*.java src/jdm/service/*.java src/jdm/alert/*.java
-java -cp "out;lib/*" jdm.Main
+java -cp "out:lib/*" jdm.Main
 read
